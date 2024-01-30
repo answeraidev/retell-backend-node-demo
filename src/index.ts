e88039a5a2f4bd58f3764b39f73296dbc51034c9
Zexia Zhang <zexia.austin@gmail.com>
2024-01-30 13:45:03 -0800

    env file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Server } from "./server";
 import { RegisterTwilioApi } from "./twilio_api";
+import dotenv from "dotenv";
+
+// Load up env file which contains credentials
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const server = new Server();
 
-RegisterTwilioApi(server.app);
+// RegisterTwilioApi(server.app);
 
 server.listen(8080);
