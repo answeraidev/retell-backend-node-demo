b6c379789021558d442810f3683e4e2adc2d2477
Zexia Zhang <zexia.austin@gmail.com>
2024-01-30 21:56:43 -0800

    use sdk

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import { Server } from "./server";
-import { RegisterTwilioApi } from "./twilio_api";
 import dotenv from "dotenv";
-
 // Load up env file which contains credentials
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+import { Server } from "./server";
 
 const server = new Server();
 
--- src/twilio_api.ts
+++ src/twilio_api.ts
@@ -3,18 +3,22 @@ import axios from "axios";
 import VoiceResponse from "twilio/lib/twiml/VoiceResponse";
 import expressWs from "express-ws";
 import twilio from "twilio";
-import dotenv from "dotenv";
-
-// Load up env file which contains credentials
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+import { RetellClient } from "retell-sdk";
+import {
+  AudioWebsocketProtocol,
+  AudioEncoding,
+} from "retell-sdk/models/components";
 
 const twilioClient = twilio(
   process.env.TWILIO_ACCOUNT_ID,
   process.env.TWILIO_AUTH_TOKEN,
 );
 
+const retellClient = new RetellClient({
+  apiKey: process.env.RETELL_API_KEY,
+});
+
 const ipAddress = "YOUR_NGROK_ADDRESS";
-const retellAddress = "https://api.re-tell.ai";
 const retellWsAddress = "api.re-tell.ai";
 
 // Todo: add hangup and call transfer
@@ -43,29 +47,21 @@ export const DeletePhoneNumber = async (phoneNumberKey: string) => {
 };
 
 export const CreatePhoneCall = async (fromNumber: string, toNumber: string) => {
-  let call: any;
   try {
-    let response = await axios({
-      url: `${retellAddress}/register-call`,
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${process.env.RETELL_API_KEY}`,
-        "Content-type": "application/json",
-      },
-      data: {
-        agent_id: process.env.RETELL_AGENT_ID,
-        audio_websocket_protocol: "twilio",
-        audio_encoding: "mulaw",
-        sample_rate: 8000,
-      },
+    const res = await retellClient.registerCall({
+      agentId: process.env.RETELL_AGENT_ID,
+      audioWebsocketProtocol: AudioWebsocketProtocol.Twilio,
+      audioEncoding: AudioEncoding.Mulaw,
+      sampleRate: 8000,
     });
-    call = response.data;
-    await twilioClient.calls.create({
-      url: `${ipAddress}/twilio-voice-webhook?callId=${call.call_id}`,
-      to: toNumber,
-      from: fromNumber,
-    });
-    console.log(`Call from: ${fromNumber} to: ${toNumber}`);
+    if (res.callDetail) {
+      await twilioClient.calls.create({
+        url: `${ipAddress}/twilio-voice-webhook?callId=${res.callDetail.callId}`,
+        to: toNumber,
+        from: fromNumber,
+      });
+      console.log(`Call from: ${fromNumber} to: ${toNumber}`);
+    }
   } catch (error: any) {
     console.error("failer to retrieve caller information: ", error);
   }
@@ -74,32 +70,23 @@ export const CreatePhoneCall = async (fromNumber: string, toNumber: string) => {
 export const RegisterTwilioApi = (app: expressWs.Application) => {
   // Twilio voice webhook
   app.post("/twilio-voice-webhook", async (req: Request, res: Response) => {
-    let callId = "";
     try {
-      let res2 = await axios({
-        url: `${retellAddress}/register-call`,
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${process.env.RETELL_API_KEY}`,
-          "Content-type": "application/json",
-        },
-        data: {
-          agent_id: process.env.RETELL_AGENT_ID,
-          audio_websocket_protocol: "twilio",
-          audio_encoding: "mulaw",
-          sample_rate: 8000,
-        },
+      const callResponse = await retellClient.registerCall({
+        agentId: process.env.RETELL_AGENT_ID,
+        audioWebsocketProtocol: AudioWebsocketProtocol.Twilio,
+        audioEncoding: AudioEncoding.Mulaw,
+        sampleRate: 8000,
       });
-      callId = res2.data.call_id;
-
-      // Start phone call websocket
-      const response = new VoiceResponse();
-      const start = response.connect();
-      const stream = start.stream({
-        url: `wss://${retellWsAddress}/audio-websocket/${callId}`,
-      });
-      res.set("Content-Type", "text/xml");
-      res.send(response.toString());
+      if (callResponse.callDetail) {
+        // Start phone call websocket
+        const response = new VoiceResponse();
+        const start = response.connect();
+        const stream = start.stream({
+          url: `wss://${retellWsAddress}/audio-websocket/${callResponse.callDetail.callId}`,
+        });
+        res.set("Content-Type", "text/xml");
+        res.send(response.toString());
+      }
     } catch (err) {
       console.error("Error in twilio voice webhook:", err);
       res.status(500).send();
